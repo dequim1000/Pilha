15cd3a322e65a93182b34715f73d8b334a144aa0
André Otávio <49661887+dequim1000@users.noreply.github.com>
2021-01-16 23:11:02 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Pilha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new Pilha();
+            //Empilhar
+            Console.WriteLine("Empilhando Itens");
+
+            num.Empilha(1);
+            num.Empilha(10);
+            num.Empilha(100);
+            num.Empilha(120);
+            num.Empilha(174);
+            num.Empilha(204);
+
+            //Desempilhar do Topo
+            Console.WriteLine("Itens Desempilhados");
+            Console.WriteLine(num.Desempilha());
+            Console.WriteLine(num.Desempilha());
+        }
+    }
+}
